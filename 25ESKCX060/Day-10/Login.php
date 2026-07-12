7349e0de6e0804e671b5eb8dbd6fcbb3b38efd6a
ahujamanish701-sys <ahujamanish701@gmail.com>
2026-07-12 16:41:44 +0530

    Update Login.php

--- 25ESKCX060/Day-10/Login.php
+++ 25ESKCX060/Day-10/Login.php
@@ -1,8 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
 
-<?php
-include("header.php");
-include("checkRegisterationError.php");
-?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
 
 <div class="container mt-5" style="max-width:500px;">
 
@@ -29,7 +33,6 @@ Login
 </form>
 
 </div>
-<?php
-include("footer.php");
-?>
 
+</body>
+</html>
